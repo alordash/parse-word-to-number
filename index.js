15b890fb6088550a04418817d0eb12d68b86ac4e
alordash <54469554+alordash@users.noreply.github.com>
2020-08-09 21:41:25 +0300

    Replaced require with fs, added new expressions

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const { Parser } = require('./lib/parse-word-to-number');
 let parser = new Parser();
 console.log(JSON.stringify(parser.parseWord("одын")));
-const fs = require('fs');
-let table = fs.readFileSync('./lib/expressions/Expressions - RU.csv', { encoding: 'utf8' }).split(/\r\n/);
-console.log(table);
 console.log("exiting ...");
