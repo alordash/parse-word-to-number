4952d5839a7692988a277a4462528c34025ba9ca
alordash <54469554+alordash@users.noreply.github.com>
2020-08-13 01:04:28 +0300

    Numbers of multiply_level > 1 can multiply itself once

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const { Parser } = require('./lib/parse-word-to-number');
 let parser = new Parser();
 console.log(JSON.stringify(parser.parseWord("twenty-one")));
 console.log(JSON.stringify(parser.parseWord("melleon")));
-console.log(parser.parseString("eleven twelve trillions nine-thousand nine nine-millions eight-hundred-seventy-seven thousands six-hundred-fivety-four"));
+console.log(parser.parseString("two-hundred three-hundred four-hundred five hundreds hundred hundred hundred hundred hundred"));
 //console.log(parser.parseString("two-hundred three-hundred four-hundred five hundreds hundred hundred hundred hundred hundred"));
 console.log("exiting ...");
